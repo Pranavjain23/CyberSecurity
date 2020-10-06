3b1a3eb8232a1a43329dab81aad9c60cfe6e9773
Bhoomika <bhoomika.garg.9@gmail.com>
2020-10-06 14:57:38 +0530

    App finalising-home fit to screen left

--- app/src/main/java/com/csao/cybersecurity/fragment/AppCreatorFragment.kt
+++ app/src/main/java/com/csao/cybersecurity/fragment/AppCreatorFragment.kt
@@ -1,10 +1,12 @@
 package com.csao.cybersecurity.fragment
 
 import android.os.Bundle
+import android.text.method.LinkMovementMethod
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import com.csao.cybersecurity.R
 import com.csao.cybersecurity.activity.MainActivity
 
@@ -18,6 +20,13 @@ class AppCreatorFragment : Fragment() {
         // Inflate the layout for this fragment
         val view = inflater.inflate(R.layout.fragment_app_creator, container, false)
         getActionBar()?.setTitle(getString(R.string.app_creator_team));
+
+
+        val link = view.findViewById<TextView>(R.id.txtContactNumber)
+        link.movementMethod = LinkMovementMethod.getInstance()
+        val link10 = view.findViewById<TextView>(R.id.txtContactNumber1)
+        link10.movementMethod = LinkMovementMethod.getInstance()
+
         return view
     }
     private fun getActionBar(): androidx.appcompat.app.ActionBar? {

ad4af8b7c90abeecf839651d8bc3cfeb8cc93a0f
Bhoomika <bhoomika.garg.9@gmail.com>
2020-10-28 12:35:36 +0530

    Locale Lang UI and coding

--- app/src/main/java/com/csao/cybersecurity/fragment/Language.kt
+++ app/src/main/java/com/csao/cybersecurity/fragment/Language.kt
@@ -4,19 +4,32 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import androidx.core.app.ActivityCompat.recreate
 import androidx.fragment.app.Fragment
 import com.csao.cybersecurity.R
 import com.csao.cybersecurity.activity.MainActivity
+import com.csao.cybersecurity.model.LocaleHelper
 
 
 class Language : Fragment() {
+    lateinit var btnHindi:Button
+     private  var mLanguageCode:String? = "hi"
 
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
+            inflater: LayoutInflater, container: ViewGroup?,
+            savedInstanceState: Bundle?
     ): View? {
         val view= inflater.inflate(R.layout.fragment_language, container, false)
         getActionBar()?.setTitle(getString(R.string.lang))
+        btnHindi=view.findViewById(R.id.btn1)
+
+        btnHindi.setOnClickListener {
+            LocaleHelper.setLocale(context, mLanguageCode);
+
+            //It is required to recreate the activity to reflect the change in UI.
+           // recreate(MainActivity())
+        }
 
         return view;
     }
